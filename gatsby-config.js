2e70b43bf8d4f15b99e4f0fb044deb4c7d57aa3f
Andzej Kogut <andrzej.kogut@bpol.net>
2022-03-09 14:56:26 +0100

    eslint fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-      title: ``,
-    siteUrl: `https://www.yourdomain.tld`
+    title: '',
+    siteUrl: 'https://www.yourdomain.tld',
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
+  plugins: ['gatsby-plugin-styled-components', 'gatsby-plugin-react-helmet'],
 };
